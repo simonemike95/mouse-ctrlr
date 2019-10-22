4d5e5a471aa2e34e949c7e3cf701a7ca060aa29a
Mike Simone <mike@msimone.dev>
2019-10-22 18:20:07 -0400

    Mouse can now move anywhere on available display
    FAILSAFE also allows the process to terminate when the mouse is moved into any corner of the screen.

--- MouseCtrlr.py
+++ MouseCtrlr.py
@@ -1,10 +1,13 @@
 import pyautogui
 import random
 
+# Moving cursor to top left of screen will kill the script
+pyautogui.FAILSAFE = True
+
 try:
     while 1:
-        xCurr = random.randint(100, 250)
-        yCurr = random.randint(100, 250)
+        xCurr = random.randint(0, pyautogui.size().width - 100)
+        yCurr = random.randint(0, pyautogui.size().height - 100)
         pyautogui.moveTo(xCurr, yCurr, duration = ((xCurr + yCurr) / 120))
         pyautogui.press('shift')
 except KeyboardInterrupt:
