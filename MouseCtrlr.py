1b0daeb3cf40744edaa3aabc090b3f1fc755b8b9
Mike Simone <mike@msimone.dev>
2021-01-08 09:56:03 -0500

    Added a sleep

--- MouseCtrlr.py
+++ MouseCtrlr.py
@@ -1,5 +1,6 @@
 import pyautogui
 import random
+import time
 
 # Moving cursor to top left of screen will kill the script
 pyautogui.FAILSAFE = True
@@ -10,5 +11,6 @@ try:
         yCurr = random.randint(0, pyautogui.size().height - 100)
         pyautogui.moveTo(xCurr, yCurr, duration = ((xCurr + yCurr) / 120))
         pyautogui.press('shift')
+        time.sleep(10)
 except KeyboardInterrupt:
     print('Key interrupt, stopping...')
